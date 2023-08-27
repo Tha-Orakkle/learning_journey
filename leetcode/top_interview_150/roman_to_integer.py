16a9a1a1f25e81e81fbb663f16728d9896707144
Tha-Orakkle <adegbiranayinoluwa.paul@yahoo.com>
2023-08-28 00:13:54 +0100

    converts roman numerals to integers using Python

--- leetcode/top_interview_150/roman_to_integer.py
+++ leetcode/top_interview_150/roman_to_integer.py
@@ -8,7 +8,8 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        dicto = {"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
+        dicto = {"I": 1, "V": 5, "X": 10, "L": 50,
+                 "C": 100, "D": 500, "M": 1000}
         result = dicto.get(s[0])
 
         for i in range(1, len(s)):
